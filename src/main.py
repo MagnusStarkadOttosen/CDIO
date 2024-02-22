f1735c9981cb6496f179ffed43de5c46587bd080
Melissa Woo <s224311@dtu.dk>
2024-02-22 15:15:19 +0100

    moving images genration out of src

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from camera.filters import filter_image
-from camera.filters import save_image
+import cv2
+import numpy as np
+
+from camera.filters import apply_gray
+#from ..testing.visualization import draw_circles, draw_lines, save_detection_image
+
 #Path from where images comes from and path where the processed images are stored
 input_folder_path = 'images/'
 output_folder_path = 'images/'
 
 #Name of the image to be used
-image_name = 'bowl.jpg'
-
+image_name = '1.jpg'
 input_image_path = input_folder_path + image_name
 
-image = filter_image(input_image_path)
-save_image('bowl.JPG', output_folder_path, image)
+image = cv2.imread(input_image_path)
+gray = apply_gray(image)
+#save_detection_image(image)
+
+#save_image("bowl', output_folder_path, image)
--- testing/visualization.py
+++ testing/visualization.py
@@ -2,9 +2,16 @@ import cv2
 import numpy as np
 
 from src.camera.object_detection import detect_balls, detect_walls
+def save_detection_image(image_name, output_folder_path, image):
+  draw_circles(image)
+  draw_lines(image)
+  output_image_name = 'processed_' + image_name
+  output_image_path = output_folder_path + output_image_name
+  cv2.imwrite(output_image_path, image)
+  print(f"Processed image saved at: {output_image_path}")
 
-def generate_circles(image):
-  circles = generate_circles(image)
+def draw_circles(image):
+  circles = detect_balls(image)
   if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
@@ -14,18 +21,13 @@ def generate_circles(image):
         # circle outline
         radius = i[2]
         cv2.circle(image, center, radius, (255, 0, 255), 3)
+  
 
-def generate_lines(image):
-  linesP = generate_lines(image)
+def draw_lines(image):
+  linesP = detect_walls(image)
   if linesP is not None:
       for i in range(0, len(linesP)):
           l = linesP[i][0]
           cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
   else:
     print("Lines are empty.")
-
-def save_detection_image(image_name, output_folder_path, image):
-  output_image_name = 'processed_' + image_name
-  output_image_path = output_folder_path + output_image_name
-  cv2.imwrite(output_image_path, image)
-  print(f"Processed image saved at: {output_image_path}")
