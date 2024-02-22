1a5dd840045fa94ae2f0635945b2e2fb4ba2441b
Melissa Woo <s224311@dtu.dk>
2024-02-22 14:03:20 +0100

    adding a detect_balls and detect_walls function

--- src/camera/object_detection.py
+++ src/camera/object_detection.py
@@ -2,3 +2,12 @@ import cv2
 import numpy as np
 
 from filters import apply_gray, apply_canny, apply_blur
+
+def detect_balls(image): #IMAGE NEEDS TO BE FILTERS GRAY
+  rows = image.shape[0]
+  blurred = apply_blur(image)
+  circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, rows / 8,param1=100, param2=30,minRadius=10, maxRadius=100)
+
+def detect_walls(image): 
+   canny = apply_canny(image)
+   linesP = cv2.HoughLinesP(canny, 1, np.pi / 180, 50, None, 50, 10)
