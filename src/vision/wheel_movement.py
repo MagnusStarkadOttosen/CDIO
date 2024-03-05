669837cb277feb2207a2551a0a2b5d78597082f2
Melissa Woo <s224311@dtu.dk>
2024-03-05 12:28:41 +0100

    incorporating tested code for wheel rotation into og function

--- src/vision/wheel_movement.py
+++ src/vision/wheel_movement.py
@@ -11,9 +11,10 @@ class Pos:
         self.y = pos_y
 
 
-def wheel_rotation():
-    wheel_circumferenc_cm = math.pi * WHEEL_DIMENSION
-    rotations = DISTANCE_TO_MOVE / wheel_circumferenc_cm
+# def move_robot
+
+def get_wheel_rotation(distance_to_move):
+    rotations = distance_to_move / WHEEL_CIRCUMF_CM
     degrees_to_move = rotations * 360
     return degrees_to_move
 
@@ -23,9 +24,3 @@ def get_distance_to_move(robot_pos: Pos, target_pos: Pos):
     dist_y = abs(robot_pos.y - target_pos.y)
     distance = round(math.sqrt(dist_x ** 2 + dist_y ** 2), 1)
     return distance
-
-
-def get_wheel_rotation(distance_to_move):
-    rotations = distance_to_move / WHEEL_CIRCUMF_CM
-    degrees_to_move = rotations * 360
-    return degrees_to_move
