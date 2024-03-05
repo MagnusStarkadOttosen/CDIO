938b9ed011e0a0d5c9cbff7c15bb7dad55cfcf3d
Melissa Woo <s224311@dtu.dk>
2024-03-05 11:57:11 +0100

    adding a test for get_distance_to_move function

--- Tests/test_wheel_movement.py
+++ Tests/test_wheel_movement.py
@@ -1,7 +1,9 @@
 import unittest
 from src.vision.wheel_movement import wheel_rotation, WHEEL_DIMENSION, DISTANCE_TO_MOVE
+from src.vision.wheel_movement import get_distance_to_move, Pos
 import math
 import sys
+
 sys.path.append('../src')
 
 
@@ -9,7 +11,19 @@ class TestWheel(unittest.TestCase):
     def test_wheel_rotation(self):
         expected_degrees = (DISTANCE_TO_MOVE / (math.pi * WHEEL_DIMENSION)) * 360
         self.assertAlmostEqual(wheel_rotation(), expected_degrees)
-        print('Expectet degree: ' , expected_degrees)
+        print('Expectet degree: ', expected_degrees)
+
+
+class TestDistanceCalculator(unittest.TestCase):
+    def test_distance_to_move(self):
+        expected_distance = 8.1
+        pos_robot = Pos(3, 4)
+        pos_ball = Pos(7, 9)
+        actual_distance = get_distance_to_move(pos_robot, pos_ball)
+        self.assertAlmostEqual(actual_distance, expected_distance)
+        print('Expected distance: ', expected_distance,
+              'Actual distance: ', actual_distance
+              )
 
 
 if __name__ == '__main__':
