83025070c06b0b609625d449001e70e797ada727
Melissa Woo <s224311@dtu.dk>
2024-03-10 16:25:46 +0100

    tidying up unused code

--- src/vision/wheel_movement.py
+++ src/vision/wheel_movement.py
@@ -3,10 +3,6 @@ import math
 
 def get_distance_to_move(robot_pos, target_pos):
     dist_vector = robot_pos - target_pos
-    print('Calculate dist vector: ', dist_vector)
     distance = round(math.sqrt(dist_vector[0] ** 2 + dist_vector[1] ** 2), 1)
-    """dist_x = robot_pos.x - target_pos.x
-    dist_y = robot_pos.y - target_pos.y
-    distance = round(math.sqrt(dist_x ** 2 + dist_y ** 2), 1)"""
     return distance
 
