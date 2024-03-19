2c0215362428b73d4df32ff1f0e2a167ad2e6335
Melissa Woo <s224311@dtu.dk>
2024-03-19 21:57:34 +0100

    looping over test commands to send to server and wait for response

--- src/client/pc_client.py
+++ src/client/pc_client.py
@@ -12,8 +12,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sock.connect(ev3_address)
     print("Connected to EV3. Type 'exit' to quit.")
-
-    while True:
+    test_commands = ["move 5", "turn 90", "move 8"]
+    for command in test_commands:
+        sock.sendall(command.encode('utf-8'))
+        response = sock.recv(buffer_size)
+        print("Response from server:", response.decode('utf-8'))
+    """while True:
         command = input("Enter command: ")
         if command:
             sock.sendall(command.encode('utf-8'))
@@ -21,7 +25,7 @@ try:
             if command == "exit":
                 break
         else:
-            print("Please enter a command.")
+            print("Please enter a command.")"""
 except socket.gaierror as e:
     print(f"Error connecting to EV3: {e}")
 finally:
