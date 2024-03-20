18f668dfc80ae6a4743b5d831c02d050296c146e
Melissa Woo <s224311@dtu.dk>
2024-03-20 08:59:05 +0100

    test commands on robot

---
+++ src/client/test_commands.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_D, SpeedPercent
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+tank_drive.on_for_seconds(SpeedPercent(30), SpeedPercent(30), 2)
+
+
+
