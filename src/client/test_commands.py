80b8ff418a1c456465241aae88690875cf68ca94
Melissa Woo <s224311@dtu.dk>
2024-03-20 09:04:55 +0100

    adding backwards test command

--- src/client/test_commands.py
+++ src/client/test_commands.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_D, SpeedPercent
+from ev3dev2.motor import (MoveTank, OUTPUT_A
+, OUTPUT_D, SpeedPercent)
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
 tank_drive.on_for_seconds(SpeedPercent(30), SpeedPercent(30), 2)
+tank_drive.on_for_seconds(SpeedPercent(-30), SpeedPercent(-30), 2)
 
 
 
