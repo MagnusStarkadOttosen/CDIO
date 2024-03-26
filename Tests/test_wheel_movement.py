f63a861ab43c5a232a452efd8b992e3ac2f4d10b
Camellia <91070768+s203237@users.noreply.github.com>
2024-03-26 10:43:39 +0100

    update

--- Tests/test_wheel_movement.py
+++ Tests/test_wheel_movement.py
@@ -3,39 +3,33 @@ import os
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_path)
 import unittest
-from src.vision.wheel_movement import get_distance_to_move, WHEEL_CIRCUMF_CM, get_wheel_rotation
+from src.vision.wheel_movement import get_distance_to_move
 from src.vision.shape_detection import Pos, Robot
 from src.vision.wheel_movement import get_degrees_to_rotation
 
 
 
 
-
-""""
-class TestWheel(unittest.TestCase):
-    def test_wheel_rotation(self):
-        distance_to_move = 70  # For test
-        expected_degrees = (distance_to_move / WHEEL_CIRCUMF_CM) * 360
-        self.assertAlmostEqual(get_wheel_rotation(distance_to_move), expected_degrees)
-        print('Expected degree: ', expected_degrees)
-
-
+# test get_distance_to_move
 class TestDistanceCalculator(unittest.TestCase):
     def test_distance_to_move(self):
-        expected_distance = 8.1
-        pos_robot = Pos(3, 2)
-        pos_ball = Pos(7, 9)
-        actual_distance = get_distance_to_move(pos_robot, pos_ball)
+        robot = Robot()
+        robot.M = (3, 2)
+        robot.B = (7, 9)
+        expected_distance = 8.06
+        actual_distance = get_distance_to_move(robot, robot.B)
         self.assertAlmostEqual(actual_distance, expected_distance)
+        print('Expected distance: ', expected_distance)
 
     def test_distance_to_move_negative(self):
-        expected_distance = 7.3
-        pos_robot = Pos(5, 2)
-        pos_ball = Pos(7, 9)
-        actual_distance = get_distance_to_move(pos_robot, pos_ball)
+        robot = Robot()
+        robot.M = (5, 2)
+        robot.B = (7, 9)
+        expected_distance = 7.28
+        actual_distance = get_distance_to_move(robot, robot.B)
         self.assertAlmostEqual(actual_distance, expected_distance)
+        print('Expected distance: ', expected_distance)
 
-"""
 # test get_degrees_to_rotation
 class TestRobotRotationCalculator(unittest.TestCase):
     def test_robot_rotation(self):
--- src/vision/wheel_movement.py
+++ src/vision/wheel_movement.py
@@ -9,30 +9,10 @@ WHEEL_CIRCUMF_CM = WHEEL_DIMENSION * math.pi
 #def get_degrees_to_pivot(current_robot_orientation):
     #current_robot_orientation
 
-def get_wheel_rotation(distance_to_move):
-    rotations = distance_to_move / WHEEL_CIRCUMF_CM
-    degrees_to_move = rotations * 360
-    return degrees_to_move
-
-
-def get_distance_to_move(robot_pos: Pos, target_pos: Pos):
-    dist_x = robot_pos.x - target_pos.x
-    dist_y = robot_pos.y - target_pos.y
-    distance = round(math.sqrt(dist_x ** 2 + dist_y ** 2), 1)
-    return distance
-
-def get_degrees_to_pivot(robot_pos: Pos, target_pos: Pos):
-    
-    dist_x = robot_pos.x - target_pos.x
-    dist_y = robot_pos.y - target_pos.y
-    angle = math.degrees(math.atan2(dist_y, dist_x))
-    return angle
-
-
+# calculate the distance the robot needs to move to reach the target position
 def get_distance_to_move(self, target_pos):
         b = math.sqrt((target_pos[0] - self.M[0]) ** 2 + (target_pos[1] - self.M[1]) ** 2)
         return b
-
 
 # calculate the degrees between the vector MB and the vector MC
 # determine the robot's rotation direction relative to the target position ( turn left or turn right)
