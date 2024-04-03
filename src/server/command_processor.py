91ba302e4dda108033ecbae725057eed19c461f4
Melissa Woo <s224311@dtu.dk>
2024-04-03 16:45:21 +0200

    putting actions in a dictionary to simplify if else statements

--- src/server/command_processor.py
+++ src/server/command_processor.py
@@ -4,29 +4,26 @@ from src.server.robot import Robot
 from src.server.robot_stub import RobotStub
 
 robot = RobotStub()  # Robot()
+actions_functions = {
+    "move": robot.drive,
+    "turn": robot.turn_by_x_degrees,
+    "start_collect": robot.run_collector_clockwise,
+    "deliver": robot.run_collector_counterclockwise,
+    "stop_collect": robot.stop_collector
+}
 
 
 def process_command(command):
-    command_list = command.split(" ")
+    command_parsed = command.split(" ")
     try:
-        action = command_list[0].lower()
-        if len(command_list) > 1:
-            value = float(command_list[1])
-            if action == "move":
-                robot.drive(value)
-            elif action == "turn":
-                robot.turn_by_x_degrees(value)
+        action = command_parsed[0].lower()
+        if action in actions_functions:
+            if len(command_parsed) > 1:
+                value = float(command_parsed[1])
+                actions_functions[action](value)
             else:
-                print("Invalid")
+                actions_functions[action]()
         else:
-            if action == "start_collect":
-                robot.run_collector_clockwise()
-            elif action == "deliver":
-                robot.run_collector_counterclockwise()
-            elif action == "stop_collect":
-                robot.stop_collector()
-            else:
-                print('Invalid command')  # perhaps remove this and handle command formatting errors client-side
-
+            print("Invalid command")
     except IndexError as e:
         print("Command should be in the format 'command int':", e)
