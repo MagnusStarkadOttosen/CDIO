b266ff110aeb97117bfc68d27ed66218789930c2
Melissa Woo <s224311@dtu.dk>
2024-04-03 09:38:33 +0200

    adding temporary red_point and green_point for testing robot position and adding image with robot on it to test

--- src/client/pathFinder.py
+++ src/client/pathFinder.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 from src.vision.image_measurement import convert_px_to_cm
-from src.vision.shape_detection import Shapes
+# from src.vision.shape_detection import Shapes
+from src.vision.detector_robot import detect_ball
 from src.vision.wheel_movement import get_distance_to_move,get_degrees_to_rotation
 
 class Pos:
@@ -17,24 +18,22 @@ class Route:
         self.newAngle = newAngle
         self.drivingMode = drivingMode
 
-def balls_are_remaining(shapes):
-    if shapes.circles is not None:
+def balls_are_remaining(circles):
+    if circles is not None:
         return True
     else:
         return False
 
 
-def findNearestBall(robotpostition:Pos, shape:Shapes):
+def findNearestBall(robotpostition:Pos, circles):
     ball_route = Route(0, 0, 0, 0, " ")
-    circles = np.round(shape.circles[0, :]).astype("int")
     if balls_are_remaining:
         nearest = 300000
         for (x, y, r) in circles:
             # width_cm, height_cm = convert_px_cm(circle.x, circle.y)
             # ball = np.array([width_cm, height_cm])
             # print(f"width : {width_cm} heigth: {height_cm}")
-            print
-            dist=get_distance_to_move((robotpostition.M[0],robotpostition.M[1]), np.array([x, y]))
+            dist=get_distance_to_move((robotpostition[0],robotpostition[1]), np.array([x, y]))
             print("dist before if: ", dist)
             if(dist<nearest):
                 ball_route.x=x
@@ -47,7 +46,7 @@ def findNearestBall(robotpostition:Pos, shape:Shapes):
     else:
         return 0
 
-def findNearestWall(robotpostition:Pos,shape:Shapes,route:Route):
+"""def findNearestWall(robotpostition:Pos,shape:Shapes,route:Route):
     nearest = 300
     lines = np.round(shape.lines[0, :]).astype("int")
 
@@ -59,13 +58,13 @@ def findNearestWall(robotpostition:Pos,shape:Shapes,route:Route):
             route.x=x
             route.y=y
             route.d=dist
-
 def roboDrive(route:Route,pos:Pos,shape:Shapes):
    findNearestBall(pos,shape,route)
    route.drivingmode="roboDrive"
    return route
-def straightDrive(robotPostion,shape:Shapes):
-    route=findNearestBall(robotPostion,shape)
+"""
+def straightDrive(robotPostion,circles):
+    route=findNearestBall(robotPostion,circles)
     route.drivingmode="straightDrive"
     route.newAngle=get_degrees_to_rotation(robotPostion,(route.x,route.y))
 
