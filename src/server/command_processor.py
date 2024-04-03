0d0cfce72921d79d592e7b5b97a3eb0bf00b14ac
Melissa Woo <s224311@dtu.dk>
2024-04-03 16:21:59 +0200

    adding start_collect, deliver and stop_collect to command options and putting move and rotate under an if statement to check if command has a value

--- src/server/command_processor.py
+++ src/server/command_processor.py
@@ -2,22 +2,31 @@ import time
 
 from src.server.robot import Robot
 from src.server.robot_stub import RobotStub
-robot = RobotStub() #Robot()
+
+robot = RobotStub()  # Robot()
 
 
 def process_command(command):
     command_list = command.split(" ")
     try:
         action = command_list[0].lower()
-
-        value = float(command_list[1])
-
-        if action == "move":
-            robot.drive(value)
-        elif action == "rotate":
-            robot.turn_by_x_degrees(value)
+        if len(command_list) > 1:
+            value = float(command_list[1])
+            if action == "move":
+                robot.drive(value)
+            elif action == "rotate":
+                robot.turn_by_x_degrees(value)
+            else:
+                print("Invalid")
         else:
-            print('Invalid command')  # perhaps remove this and handle command formatting errors client-side
+            if action == "start_collect":
+                robot.run_collector_clockwise()
+            elif action == "deliver":
+                robot.run_collector_counterclockwise()
+            elif action == "stop_collect":
+                robot.stop_collector()
+            else:
+                print('Invalid command')  # perhaps remove this and handle command formatting errors client-side
 
     except IndexError as e:
         print("Command should be in the format 'command int':", e)
