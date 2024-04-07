ccebee35227d2ed5c1bde96a8afc3f822e548469
Melissa Woo <s224311@dtu.dk>
2024-04-07 10:46:00 +0200

    updating variable and function names for clarity

--- src/client/field/objects_on_field/robot.py
+++ src/client/field/objects_on_field/robot.py
@@ -20,52 +20,52 @@ WHEEL_CIRCUMF_CM = WHEEL_DIMENSION * math.pi
 
 class Robot:
     def __init__(self):
-        self.A = None  # red point's coordinates
-        self.B = None  # green point's coordinates
-        self.M = None  # the robot's position
-        self.pivot = 0  # the angle between the tector MB and tector MC with C is the ball's position
+        self.red_dot = None  # red point's coordinates
+        self.green_dot = None  # green point's coordinates
+        self.pos = None  # the robot's position
+        self.pivot = 0  # the angle between the vector pos->green and vector pos->ball_pos
 
-    def update_AB_andM_from_image(self, image):
-        red_point = detect_ball(filter_image_red(image))
-        green_point = detect_ball(filter_image_green(image))
+    def update_pos_from_image(self, image):
+        red = detect_ball(filter_image_red(image))
+        green = detect_ball(filter_image_green(image))
         if filter_image_red(image) is not None:
-            if red_point is not None:
-                self.A = (red_point[0], red_point[1])  # Extract x and y coordinates
+            if red is not None:
+                self.red_dot = (red[0], red[1])  # Extract x and y coordinates
             else:
-                self.A = None
+                self.red_dot = None
 
         if filter_image_green(image) is not None:
-            if green_point is not None:
-                self.B = (green_point[0], green_point[1])
+            if green is not None:
+                self.green_dot = (green[0], green[1])
             else:
-                self.B = None
+                self.green_dot = None
 
             # distance from A to B
-            distance = math.sqrt((self.B[0] - self.A[0]) ** 2 + (self.B[1] - self.A[1]) ** 2)
+            distance = math.sqrt((self.green_dot[0] - self.red_dot[0]) ** 2 + (self.green_dot[1] - self.red_dot[1]) ** 2)
             # distance from A to M :a
             a = 10
-            self.M = (red_point[0] + a / (distance * (green_point[0] - red_point[0])),
-                      red_point[1] + a / (distance * (green_point[1] - red_point[1])))
-        return self.A, self.B, self.M
+            self.pos = (red[0] + a / (distance * (green[0] - red[0])),
+                        red[1] + a / (distance * (green[1] - red[1])))
+        return self.red_dot, self.green_dot, self.pos
 
-    def update_pivot_from_image(self, target_pos):
-        self.C = target_pos
-        get_degrees_to_rotation(self, target_pos)
+    def update_pivot_from_image(self, ball_pos):
+        # self.C = target_pos # There is no member C in Robot??
+        self.pivot = self.get_degrees_to_rotate(ball_pos)
 
     def move(self, target_pos):
         # distance to move is the distance from M to the target position
         # the robot's position is updated after the robot moves
         get_distance(self, target_pos)
-        self.M = target_pos
-        return self.M
+        self.pos = target_pos
+        return self.pos
 
     def get_degrees_to_rotate(self, target_pos):
         # distance MB = a
-        a = math.sqrt((self.B[0] - self.M[0]) ** 2 + (self.B[1] - self.M[1]) ** 2)
+        a = math.sqrt((self.green_dot[0] - self.pos[0]) ** 2 + (self.green_dot[1] - self.pos[1]) ** 2)
         # distance MC = b
-        b = math.sqrt((target_pos[0] - self.M[0]) ** 2 + (target_pos[1] - self.M[1]) ** 2)
+        b = math.sqrt((target_pos[0] - self.pos[0]) ** 2 + (target_pos[1] - self.pos[1]) ** 2)
         # distance BC = c
-        c = math.sqrt((target_pos[0] - self.B[0]) ** 2 + (target_pos[1] - self.B[1]) ** 2)
+        c = math.sqrt((target_pos[0] - self.green_dot[0]) ** 2 + (target_pos[1] - self.green_dot[1]) ** 2)
         # radian of angle between vector MB and vector MC
         radian = math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
         # degrees of angle between vector MB and vector MC
@@ -73,8 +73,8 @@ class Robot:
         # determine the robot's rotation direction relative to the target position
         # turn left or turn right
         # Calculate direction vectors
-        MB = (self.B[0] - self.M[0], self.B[1] - self.M[1])
-        MTarget = (target_pos[0] - self.M[0], target_pos[1] - self.M[1])
+        MB = (self.green_dot[0] - self.pos[0], self.green_dot[1] - self.pos[1])
+        MTarget = (target_pos[0] - self.pos[0], target_pos[1] - self.pos[1])
 
         # Calculate the cross product's Z component
         cross_product_z = MB[0] * MTarget[1] - MB[1] * MTarget[0]
