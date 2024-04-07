4b6132711564127a353a05270ead683bca8032bc
Melissa Woo <s224311@dtu.dk>
2024-04-07 15:31:04 +0200

    writing out index numbers again on balls for the generated image of detect_balls results

--- src/client/vision/shape_detection.py
+++ src/client/vision/shape_detection.py
@@ -125,21 +125,22 @@ def detect_balls(image):
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1.75, minDist=7,
                                param1=30, param2=35, minRadius=10, maxRadius=30)
     if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for idx, (x, y, r) in enumerate(circles[0, :]):
-            # Draw the outer circle
-            cv2.circle(image, (x, y), r, (128, 0, 128), 2)
-            # Draw the center of the circle
-            cv2.circle(image, (x, y), 2, (128, 0, 128), 3)
-            # Enumerate the detected balls
-            cv2.putText(image, str(idx + 1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+        # circles = np.uint16(np.around(circles))
+        #     for idx, (x, y, r) in enumerate(circles[0, :]):
+        #         # Draw the outer circle
+        #         cv2.circle(image, (x, y), r, (128, 0, 128), 2)
+        #         # Draw the center of the circle
+        #         cv2.circle(image, (x, y), 2, (128, 0, 128), 3)
+        #         # Enumerate the detected balls
+        #         cv2.putText(image, str(idx + 1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
         circles = np.round(circles[0, :]).astype("int")
-        for (x, y, z) in circles:
-            # print('Ball:', str(balls))
-            # print('X:', x)
-            # print('Y:', y)
-            # print('Radius:', z)
-            balls += 1
+        # for (x, y, z) in circles:
+        #     # print('Ball:', str(balls))
+        #     # print('X:', x)
+        #     # print('Y:', y)
+        #     # print('Radius:', z)
+        #     balls += 1
+        print("balls detected: ", len(circles))
     else:
         print("No balls detected.")
 
--- testing/shape_detection_test.py
+++ testing/shape_detection_test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from src.client.vision.shape_detection import detect_balls
 from testing.image_generation import write_image_to_file
@@ -8,14 +9,16 @@ image_name = "9_balls_on_field.jpeg"
 image = cv2.imread('images/'+image_name)
 
 balls = detect_balls(image)
-
+idx = 0
 for ball in balls:
+    # print(f"{type(ball[0])},type({ball[1]}),type({ball[2]})")
     # Draw the outer circle
     cv2.circle(image, (ball[0], ball[1]), ball[2], (128, 0, 128), 2)
     # Draw the center of the circle
     cv2.circle(image, (ball[0], ball[1]), 2, (128, 0, 128), 3)
     # Enumerate the detected balls
-    # cv2.putText(image, str(idx + 1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+    cv2.putText(image, str(idx), (ball[0], ball[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+    idx += 1
     print(f"ball x: {ball[0]}, y: {ball[1]}")
 
 write_image_to_file('circles_detected_' + image_name, image)
