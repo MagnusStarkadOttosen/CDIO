712da653ae2ae0e643bf47cda5dc14123428e52e
Melissa Woo <s224311@dtu.dk>
2024-04-07 16:09:36 +0200

    separating tests into unittest classes and methods to be able to run them individually

--- testing/image_generation.py
+++ testing/image_generation.py
@@ -1,4 +1,5 @@
 import cv2
+import unittest
 
 from src.client.vision.detector_robot import detect_ball
 from src.client.vision.filters import filter_image_red, filter_image_green
@@ -11,36 +12,40 @@ output_folder_path = 'images/'
 image_name = ('1.jpg', 'image_with_robot.jpeg')
 
 
-def test_filter_image_red():
-    image = cv2.imread(input_folder_path + image_name[1])
-    filtered_image = filter_image_red(image)
-    write_image_to_file('filter_red_' + image_name[1], filtered_image)
+class TestColorFilters(unittest.TestCase):
+    def test_filter_image_red(self):
+        image = cv2.imread(input_folder_path + image_name[1])
+        filtered_image = filter_image_red(image)
+        write_image_to_file('filter_red_' + image_name[1], filtered_image)
+
+    def test_filter_image_green(self):
+        image = cv2.imread(input_folder_path + image_name[1])
+        filtered_image = filter_image_green(image)
+        write_image_to_file('filter_green_' + image_name[1], filtered_image)
 
 
-def test_filter_image_green():
-    image = cv2.imread(input_folder_path + image_name[1])
-    filtered_image = filter_image_green(image)
-    write_image_to_file('filter_green_' + image_name[1], filtered_image)
+class TestDetectBall(unittest.TestCase):
+    def test_detect_balls(self):
+        image = cv2.imread(input_folder_path + image_name[0])
+        if image is not None:
+
+            circles = detect_ball(image)
+
+            # shape_detector.detect_ball()
+
+            # shape_detector.detect_walls()
+
+            # shape_detector.draw_corners_debug(image)
+            # shape_detector.draw_coordinate_system(image)
+
+            write_image_to_file('detect_balls_' + image_name[0], image)
+
+        else:
+            print("Error: Image not found. Please check the input folder path and image name.")
 
 
-def test_detect_balls():
-    image = cv2.imread(input_folder_path + image_name[0])
-    if image is not None:
-
-        circles = detect_ball(image)
-
-        # shape_detector.detect_ball()
-
-        # shape_detector.detect_walls()
-
-        # shape_detector.draw_corners_debug(image)
-        # shape_detector.draw_coordinate_system(image)
-
-        write_image_to_file('detect_balls_' + image_name[0], image)
-
-    else:
-        print("Error: Image not found. Please check the input folder path and image name.")
-
+if __name__ == '__main__':
+    unittest.main()
 
 def write_image_to_file(output_image_name, output_img):
     output_image_name = 'processed_' + output_image_name
@@ -48,7 +53,3 @@ def write_image_to_file(output_image_name, output_img):
     print("output_image_path: ", output_image_path, "output_image_name: ", output_image_name)
     cv2.imwrite(output_image_path, output_img)
     print(f"Processed image saved at: {output_image_path}")
-
-
-test_filter_image_red()
-test_filter_image_green()
--- testing/shape_detection_test.py
+++ testing/shape_detection_test.py
@@ -1,24 +1,26 @@
+import unittest
+
 import cv2
-import numpy as np
 
 from src.client.vision.shape_detection import detect_balls
 from testing.image_generation import write_image_to_file
 
-image_name = "9_balls_on_field.jpeg"
 
-image = cv2.imread('images/'+image_name)
+class TestBallDetection(unittest.TestCase):
+    def test_detect_balls(self):
+        image_name = "9_balls_on_field.jpeg"
+        image = cv2.imread('images/' + image_name)
+        balls = detect_balls(image)
+        idx = 0
+        for ball in balls:
+            # print(f"{type(ball[0])},type({ball[1]}),type({ball[2]})")
+            # Draw the outer circle
+            cv2.circle(image, (ball[0], ball[1]), ball[2], (128, 0, 128), 2)
+            # Draw the center of the circle
+            cv2.circle(image, (ball[0], ball[1]), 2, (128, 0, 128), 3)
+            # Enumerate the detected balls
+            cv2.putText(image, str(idx), (ball[0], ball[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+            idx += 1
+            print("ball x:", ball[0], "y:", ball[1])
 
-balls = detect_balls(image)
-idx = 0
-for ball in balls:
-    # print(f"{type(ball[0])},type({ball[1]}),type({ball[2]})")
-    # Draw the outer circle
-    cv2.circle(image, (ball[0], ball[1]), ball[2], (128, 0, 128), 2)
-    # Draw the center of the circle
-    cv2.circle(image, (ball[0], ball[1]), 2, (128, 0, 128), 3)
-    # Enumerate the detected balls
-    cv2.putText(image, str(idx), (ball[0], ball[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
-    idx += 1
-    print(f"ball x: {ball[0]}, y: {ball[1]}")
-
-write_image_to_file('circles_detected_' + image_name, image)
+        write_image_to_file('circles_detected_' + image_name, image)
