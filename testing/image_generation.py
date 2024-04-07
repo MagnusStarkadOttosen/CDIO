99ef388bff9dcd345250ec437ecddfbb3011f083
Melissa Woo <s224311@dtu.dk>
2024-04-07 11:39:10 +0200

    adding test for filtering image for red elements

--- testing/image_generation.py
+++ testing/image_generation.py
@@ -1,15 +1,21 @@
 import cv2
 
 from src.client.vision.detector_robot import detect_ball
+from src.client.vision.filters import filter_image_red, filter_image_green
 
 # Path from where images comes from and path where the processed images are stored
 input_folder_path = 'images/'
 output_folder_path = 'images/'
 
 # Name of the image to be used
-image_name = 'detectballs.jpg'
+image_name = '1.jpg'
 input_image_path = input_folder_path + image_name
 image = cv2.imread(input_image_path)
+
+
+def test_filter_image_red():
+    filtered_image = filter_image_red(image)
+    write_image_to_file("filter_red", filtered_image)
 
 
 def test_detect_balls():
@@ -24,14 +30,18 @@ def test_detect_balls():
         # shape_detector.draw_corners_debug(image)
         # shape_detector.draw_coordinate_system(image)
 
-        output_image_name = 'processed_' + image_name
-        output_image_path = output_folder_path + output_image_name
-
-        output_image_name_red = "Processed_img_red" + image_name
-        output_image_path_red = output_folder_path + output_image_name_red
-
-        cv2.imwrite(output_image_path, image)
-        print(f"Processed image saved at: {output_image_path}")
+        write_image_to_file("detect_balls", image)
 
     else:
         print("Error: Image not found. Please check the input folder path and image name.")
+
+
+def write_image_to_file(prefix, output_img):
+    output_image_name = 'processed_' + prefix + '_' + image_name
+    output_image_path = output_folder_path + output_image_name
+    print("output_image_path: ", output_image_path, "output_image_name: ", output_image_name)
+    cv2.imwrite(output_image_path, output_img)
+    print(f"Processed image saved at: {output_image_path}")
+
+
+test_filter_image_red()
