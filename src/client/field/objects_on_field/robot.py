0b5214a4d7bcb7016573782e126d08f24adaf3b1
Melissa Woo <s224311@dtu.dk>
2024-04-07 22:11:30 +0200

    dissolving Robot class and calculating degrees to rotate using robot_pos, green_dot and target_pos parameter instead of members of the Robot class

--- src/client/field/objects_on_field/robot.py
+++ src/client/field/objects_on_field/robot.py
@@ -1,7 +1,4 @@
 import math
-from src.client.vision.shape_detection import detect_balls
-from src.client.vision.filters import filter_image_red, filter_image_green
-from src.client.utilities import get_distance
 
 WHEEL_DIMENSION = 5.5
 DIST_BETWEEN_WHEELS = 13.5
@@ -26,7 +23,6 @@ WHEEL_CIRCUMF_CM = WHEEL_DIMENSION * math.pi
 #         self.pivot = 0  # the angle between the vector pos->green and vector pos->ball_pos
 
 def calc_robot_pos(green_dot, red_dot):
-    # distance from A to B, TODO make dist A->B a constant value
     distance = math.sqrt(
         (green_dot[0] - red_dot[0]) ** 2 + (green_dot[1] - red_dot[1]) ** 2)
     # distance from A to M :a
@@ -45,38 +41,44 @@ def __update_dot_positions(self, green_pos, red_pos):
         print("Green or red position missing coordinate")
 
 
-def update_pivot_from_image(self, ball_pos):
-    # self.C = target_pos # There is no member C in Robot??
-    self.pivot = self.get_degrees_to_rotate(ball_pos)
+# def update_pivot_from_image(robot_pos, ball_pos):
+#     # self.C = target_pos # There is no member C in Robot??
+#     return get_degrees_to_rotate(robot_pos, ball_pos)
+#
+#
+# def move(self, target_pos):
+#     # distance to move is the distance from M to the target position
+#     # the robot's position is updated after the robot moves
+#     get_distance(self, target_pos)
+#     self.pos = target_pos
+#     return self.pos
 
 
-def move(self, target_pos):
-    # distance to move is the distance from M to the target position
-    # the robot's position is updated after the robot moves
-    get_distance(self, target_pos)
-    self.pos = target_pos
-    return self.pos
+def calc_degrees_to_rotate(robot_pos, green_dot, target_pos):
+    # distance robot_pos -> green_dot = a
+    a = math.sqrt((green_dot[0] - robot_pos[0]) ** 2 + (green_dot[1] - robot_pos[1]) ** 2)
 
+    # distance robot_pos -> target_pos = b
+    b = math.sqrt((target_pos[0] - robot_pos[0]) ** 2 + (target_pos[1] - robot_pos[1]) ** 2)
 
-def get_degrees_to_rotate(self, target_pos):
-    # distance MB = a
-    a = math.sqrt((self.green_dot[0] - self.pos[0]) ** 2 + (self.green_dot[1] - self.pos[1]) ** 2)
-    # distance MC = b
-    b = math.sqrt((target_pos[0] - self.pos[0]) ** 2 + (target_pos[1] - self.pos[1]) ** 2)
-    # distance BC = c
-    c = math.sqrt((target_pos[0] - self.green_dot[0]) ** 2 + (target_pos[1] - self.green_dot[1]) ** 2)
-    # radian of angle between vector MB and vector MC
-    radian = math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
-    # degrees of angle between vector MB and vector MC
+    # distance green_dot -> target_pos = c
+    c = math.sqrt((target_pos[0] - green_dot[0]) ** 2 + (target_pos[1] - green_dot[1]) ** 2)
+
+    # radian of angle between vector pos_to_green_dot and vector MC
+    radian = math.acos((a ** 2 + b ** 2 - c ** 2)
+                       / (2 * a * b))
+
+    # degrees of angle between vector pos_to_green_dot and vector MC
     degrees = math.degrees(radian)
+
     # determine the robot's rotation direction relative to the target position
     # turn left or turn right
     # Calculate direction vectors
-    MB = (self.green_dot[0] - self.pos[0], self.green_dot[1] - self.pos[1])
-    MTarget = (target_pos[0] - self.pos[0], target_pos[1] - self.pos[1])
+    pos_to_green_dot = (green_dot[0] - robot_pos[0], green_dot[1] - robot_pos[1])
+    pos_to_target = (target_pos[0] - robot_pos[0], target_pos[1] - robot_pos[1])
 
     # Calculate the cross product's Z component
-    cross_product_z = MB[0] * MTarget[1] - MB[1] * MTarget[0]
+    cross_product_z = pos_to_green_dot[0] * pos_to_target[1] - pos_to_green_dot[1] * pos_to_target[0]
 
     # Determine direction based on the cross product's sign
     if cross_product_z > 0:
