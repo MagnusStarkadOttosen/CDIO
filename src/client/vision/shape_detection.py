1f01ae7345679c3eb86adeb215187e4b87b185a2
Melissa Woo <s224311@dtu.dk>
2024-04-09 16:47:27 +0200

    accessing first element in circle array when detecting red and green circles

--- src/client/vision/shape_detection.py
+++ src/client/vision/shape_detection.py
@@ -8,11 +8,18 @@ from src.client.vision.filters import apply_gray, apply_canny
 
 
 def detect_robot(image):
-    green_dot = detect_balls(filter_image_green(image))
-    red_dot = detect_balls(filter_image_red(image))
+    green_dot = detect_balls(filter_image_green(image),
+                             min_radius=25, max_radius=35)
+    if green_dot is None:
+        print("No green dot.")
 
-    robot_pos = calc_robot_pos(green_dot, red_dot)
-    robot_direction = calc_robot_direction(green_dot, robot_pos)
+    red_dot = detect_balls(filter_image_red(image),
+                           min_radius=25, max_radius=35)
+    if red_dot is None:
+        print("No red dot.")
+
+    robot_pos = calc_robot_pos(green_dot[0], red_dot[0])
+    robot_direction = calc_robot_direction(green_dot[0], robot_pos)
 
     return robot_pos, robot_direction
 
