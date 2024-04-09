b91d0b4a3566be54c24582fd52a28ee8bc1a569e
Melissa Woo <s224311@dtu.dk>
2024-04-09 09:04:25 +0200

    moving robot control test methods into evdevSimulator and deleting robot_controls module

--- testing/evdevSimulator.py
+++ testing/evdevSimulator.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
-from ev3dev2._platform.ev3 import INPUT_2
-from ev3dev2.motor import SpeedPercent
+from ev3dev2._platform.ev3 import INPUT_2, OUTPUT_A, OUTPUT_D
+from ev3dev2.motor import SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import ColorSensor
-from testing.robot_controls import drive, tank_drive
 
 # tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
 cs = ColorSensor(INPUT_2)
 
+
+def drive():
+    tank_drive.on(SpeedPercent(30), SpeedPercent(30))
+
+
+def move(distance):
+    tank_drive.on_for_degrees(SpeedPercent(40), SpeedPercent(40), distance)
+
+
+def stop():
+    tank_drive.stop()
+
+
 drive()
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+
 while True:
-    #avoid_hit_walls()
+    # avoid_hit_walls()
     if cs.color != 6:
-        tank_drive.stop()
+        stop()
         # move_robot_distance(-100)
         #
         # #get_wheel_rotation(30)
@@ -22,9 +36,9 @@ while True:
         # distance = move_robot_distance(300)
         # tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), distance)
         drive()
-        tank_drive.stop()
+        stop()
         #
         # #tank_drive.on_for_rotations(SpeedPercent(20),SpeedPercent(20))
         # #get_distance_to_move(60)
 
-    #drive()
+    # drive()
