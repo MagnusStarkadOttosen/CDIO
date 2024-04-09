9e42bf3e1694a1241bb23be43ece8d3252b1ff9e
Melissa Woo <s224311@dtu.dk>
2024-04-09 12:22:55 +0200

    adding helper functions to calculate directon vectors for robot and from robot to target

--- src/client/field/robot.py
+++ src/client/field/robot.py
@@ -35,10 +35,31 @@ WHEEL_CIRCUMF_CM = WHEEL_DIMENSION * math.pi
 #     self.pos = target_pos
 #     return self.pos
 
+def calc_robot_direction(green_dot, robot_pos):
+    direction = (green_dot[0] - robot_pos[0], green_dot[1] - robot_pos[1])
+    return direction
 
-def calc_degrees_to_rotate(robot_pos, green_dot, target_pos):
+
+def angle_between_vectors(vector_a, vector_b):
+    dot_product_ab = _dot_product(vector_a, vector_b)
+    length_a = _vector_length(vector_a)
+    length_b = _vector_length(vector_b)
+    cos_theta = dot_product_ab / (length_a * length_b)
+    theta_radians = math.acos(cos_theta)
+    return math.degrees(theta_radians)
+
+
+def _dot_product(vector_a, vector_b):
+    return sum(a * b for a, b in zip(vector_a, vector_b))
+
+
+def _vector_length(vector):
+    return math.sqrt(sum(x ** 2 for x in vector))
+
+
+def calc_degrees_to_rotate(robot_pos, green_dot, target_pos, robot_direction):
     # distance robot_pos -> green_dot = a
-    a = math.sqrt((green_dot[0] - robot_pos[0]) ** 2 + (green_dot[1] - robot_pos[1]) ** 2)
+    a = math.sqrt(robot_direction[0] ** 2 + robot_direction[1] ** 2)
 
     # distance robot_pos -> target_pos = b
     b = math.sqrt((target_pos[0] - robot_pos[0]) ** 2 + (target_pos[1] - robot_pos[1]) ** 2)
@@ -46,11 +67,14 @@ def calc_degrees_to_rotate(robot_pos, green_dot, target_pos):
     # distance green_dot -> target_pos = c
     c = math.sqrt((target_pos[0] - green_dot[0]) ** 2 + (target_pos[1] - green_dot[1]) ** 2)
 
-    # radian of angle between vector pos_to_green_dot and vector MC
+    # vector between robot_pos -> green_dot and robot_pos->target_pos
+    # vect_ab = _tuple_subtract(b, a)
+    # c = vect_ab
+    # radian of angle between vector pos_to_green_dot and vector robot_pos->target_pos
     radian = math.acos((a ** 2 + b ** 2 - c ** 2)
                        / (2 * a * b))
 
-    # degrees of angle between vector pos_to_green_dot and vector MC
+    # degrees of angle between vector pos_to_green_dot and vector robot_pos->target_pos
     degrees = math.degrees(radian)
 
     # determine the robot's rotation direction relative to the target position
@@ -69,3 +93,8 @@ def calc_degrees_to_rotate(robot_pos, green_dot, target_pos):
         return degrees
     else:
         return 0
+
+
+def _tuple_subtract(tuple1, tuple2):
+    result = tuple(a - b for a, b in zip(tuple1, tuple2))
+    return result
