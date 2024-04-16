8584c7161732fe09630f8212640c80daf199b7ba
Magnus Ottosen <magnus@ottosen.dk>
2024-04-16 15:05:27 +0200

    Plan out test

---
+++ testing/MoveRobotFromAtoB_test.py
@@ -0,0 +1,12 @@
+#steps
+#1 take image
+#2 find corners
+#3 take image
+#4 warp image
+#5 find robot
+#6 if robot at target stop robot and break to 11
+#7 calculate degrees to turn
+#8 if degrees to turn is not 0 turn robot
+#9 if robot not moving start robot
+#10 go to 3
+#11 print "robot at target point"
