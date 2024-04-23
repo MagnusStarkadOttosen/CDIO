9668b4a46be506c717b76af62d9d27a915df4052
Melissa Woo <s224311@dtu.dk>
2024-04-23 22:06:45 +0200

    adding if statement so that collect_white_balls can be reused for collecting orange ball, renaming to collect_ball and adding a boolean collect_orange_ball as a member of Main class

--- src/main.py
+++ src/main.py
@@ -1,68 +1,91 @@
+import time
+
 import cv2
 import numpy as np
 
-from src.client.field.coordinate_system import are_points_close
+from src.client.field.coordinate_system import are_points_close, find_corner_points_full, warp_perspective
 from src.client.field.field import Field
 from src.client.field.robot import calc_vector_direction, calc_degrees_to_rotate
 from src.client.pc_client import ClientPC
 from src.client.vision.camera import capture_image
-from src.client.vision.filters import filter_image_white
+from src.client.vision.filters import filter_image_white, filter_image_orange
 from src.client.vision.pathfinder import find_nearest_ball
 from src.client.vision.shape_detection import detect_balls, detect_robot
 
 WHITE_BALL_COUNT = 10
 ROBOT_CAPACITY = 6
 TOLERANCE = 1
+DST_SIZE = (1200, 1800)
 class Main:
         def __init__(self):
                 self.client = ClientPC()
                 self.balls = None
+                self.collect_orange_ball = False
+                self.target_pos = None
 
         def main_loop(self):
-                # pretend ur working with a video that has one single frame
-                # image_name = "gen_warped2_newCourse_bigDots.jpg"
-                # temp_frame = cv2.imread('images/' + image_name)
-
-
                 while len(self.balls) > WHITE_BALL_COUNT - ROBOT_CAPACITY - 1:
-                        self._collect_white_balls()
+                        self._collect_ball()
 
-                self._collect_orange_ball()
+                self.collect_orange_ball = True
+                while self.collect_orange_ball:
+                        self._collect_ball()
                 self._deliver_balls_loop()
 
                 while len(self.balls) > 0:
-                        self._collect_white_balls()
+                        self._collect_ball()
                 self._deliver_balls_loop()
+                self.client.send_command("stop_collect")
 
-        def _collect_white_balls(self):
+        def _collect_ball(self):  # TODO break up _collect_ball in smaller functions
                 capture_image("test.jpg")
                 image = cv2.imread("images/capturedImage/test.jpg")
-                robot_pos, robot_direction = detect_robot(image)
-                self.balls = detect_balls(image)
+                final_points = find_corner_points_full(image)
+                warped_img = warp_perspective(image, final_points, DST_SIZE)
 
-                target_pos = find_nearest_ball(robot_pos, self.balls)
-                target_direction = calc_vector_direction(robot_pos, target_pos)
 
-                if are_points_close(robot_pos, target_pos):
+                robot_pos, robot_direction = detect_robot(warped_img)
+
+                # Set target ball position
+                if not self.collect_orange_ball:
+                        self.balls = detect_balls(filter_image_white(warped_img))
+                        self.target_pos = find_nearest_ball(robot_pos, self.balls)
+                else:
+                        self.balls = detect_balls(filter_image_orange(warped_img))
+                        if len(self.balls) is 0:
+                                self.collect_orange_ball = False
+                                return
+                        self.target_pos = (self.balls[0][0], self.balls[0][1])
+
+                # Calculate target direction vector
+                target_direction = calc_vector_direction(robot_pos, self.target_pos)
+
+                # Check if robot arrived at target destination
+                if are_points_close(robot_pos, self.target_pos):
                         self.client.send_command("stop")
                         return
 
+                # Calculate the degrees the robot needs to turn to face robot
                 deg = calc_degrees_to_rotate(robot_direction, target_direction)
 
                 # Check if angle need to change
                 if deg < -TOLERANCE or deg > TOLERANCE:
                         self.client.send_command("turn " + deg)
-                        self.client.send_command("drive")
+                        # self.client.send_command("start_drive")
 
-        def _collect_orange_ball(self):
-                print("hei girl hei girl hei girl")
+        # def _collect_orange_ball(self):
+        #         print("hei girl hei girl hei girl")
+
 
         def _deliver_balls_loop(self):
-        # get angle to turn from current robot direction
-        # send command to drive until robot_pos = goal_pos
+            # get angle to turn from current robot direction
+            # send command to drive until robot_pos = goal_pos
+            self.client.send_command("deliver")
+            time.sleep(5) #  TODO use on_for_degrees in deliver command server-side
+            self.client.send_command("start_collect")
 
         def _run_get_white_balls_loop(self, filtered_image):  # probably needs to be video feed here
-                self.field.balls = detect_balls(filtered_image)
+                self.balls = detect_balls(filtered_image)
 
 
 
