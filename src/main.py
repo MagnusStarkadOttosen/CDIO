3bf77b8ba71a5299c3b700c214ab269de6bd9ba5
Melissa Woo <s224311@dtu.dk>
2024-06-07 09:45:10 +0200

    separating main loop into private functions of the main_loop

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import time
-
 import cv2
 import numpy as np
 
@@ -18,6 +17,11 @@ TOLERANCE = 1
 DST_SIZE = (1200, 1800)
 
 
+def _detect_initial_balls(final_points):
+    warped_img = warp_perspective(cv2.imread("images/capturedImage/test.jpg"), final_points, DST_SIZE)
+    return detect_balls(warped_img)
+
+
 class Main:
     def __init__(self):
         self.client = ClientPC()
@@ -27,102 +31,71 @@ class Main:
         self.camera = initialize_camera(index=2)
 
     def main_loop(self):
+        final_points = self._initialize_field()
+        self.balls = _detect_initial_balls(final_points)
+
+        self._collect_white_balls(final_points)
+        self._collect_and_deliver_orange_ball(final_points)
+        self._collect_remaining_balls(final_points)
+
+        self.client.send_command("stop_collect")
+
+    def _initialize_field(self):
         capture_image(self.camera, "test.jpg")
         image = cv2.imread("images/capturedImage/test.jpg")
         final_points = find_corner_points_full(image)
-        warped_img = warp_perspective(image, final_points, DST_SIZE)
-        if self.balls is None:
-            self.balls = detect_balls(warped_img)
-        while len(self.balls) > WHITE_BALL_COUNT - ROBOT_CAPACITY - 1:
-            self._collect_ball(final_points)
+        return final_points
 
+    def _collect_white_balls(self, final_points):
+        while len(self.balls) > WHITE_BALL_COUNT - ROBOT_CAPACITY - 1:
+            self._collect_ball(final_points, filter_image_white)
+
+    def _collect_and_deliver_orange_ball(self, final_points):
         self.collect_orange_ball = True
         while self.collect_orange_ball:
-            self._collect_ball(final_points)
-        self._deliver_balls_loop()
+            self._collect_ball(final_points, filter_image_orange)
+        self._deliver_balls()
 
+    def _collect_remaining_balls(self, final_points):
         while len(self.balls) > 0:
-            self._collect_ball(final_points)
-        self._deliver_balls_loop()
-        self.client.send_command("stop_collect")
+            self._collect_ball(final_points, filter_image_white)
+        self._deliver_balls()
 
-    def _collect_ball(self, final_points):  # TODO break up _collect_ball in smaller functions
+    def _collect_ball(self, final_points, filter_image):
         capture_image(self.camera, "test.jpg")
         image = cv2.imread("images/capturedImage/test.jpg")
-
         warped_img = warp_perspective(image, final_points, DST_SIZE)
 
         robot_pos, robot_direction = detect_robot(warped_img)
 
-        # Set target ball position
-        if not self.collect_orange_ball:
-            self.balls = detect_balls(filter_image_white(warped_img))
-            if self.balls is None:
+        if self.collect_orange_ball:
+            self.balls = detect_balls(filter_image_orange(warped_img))
+            if not self.balls:
+                self.collect_orange_ball = False
+                return
+            self.target_pos = self.balls[0][:2]
+        else:
+            self.balls = detect_balls(filter_image(warped_img))
+            if not self.balls:
                 return
             self.target_pos = find_nearest_ball(robot_pos, self.balls)
-        else:
-            self.balls = detect_balls(filter_image_orange(warped_img))
-            if self.balls is None:
-                self.collect_orange_ball = False
-                self.balls = detect_balls(filter_image_white(warped_img))
-                return
-            self.target_pos = (self.balls[0][0], self.balls[0][1])
 
-            self._navigate_to_target(robot_pos, robot_direction)
+        self._navigate_to_target(robot_pos, robot_direction)
 
-    def _deliver_balls_loop(self):
-        # get angle to turn from current robot direction
-        # send command to drive until robot_pos = goal_pos
+    def _deliver_balls(self):
         self.client.send_command("stop")
         self.client.send_command("deliver")
         time.sleep(5)  # TODO use on_for_degrees in deliver command server-side
         self.client.send_command("start_collect")
 
     def _navigate_to_target(self, robot_pos, robot_direction):
-        # Calculate target direction vector
         target_direction = calc_vector_direction(robot_pos, self.target_pos)
 
-        # Check if robot arrived at target destination
         if are_points_close(robot_pos, self.target_pos):
             self.client.send_command("stop")
             return
 
-        # Calculate the degrees the robot needs to turn to face robot
         deg = calc_degrees_to_rotate(robot_direction, target_direction)
 
-        # Check if angle need to change
-        if deg < -TOLERANCE or deg > TOLERANCE:
-            self.client.send_command("turn " + deg)
-            # self.client.send_command("start_drive")
-
-    def _run_get_white_balls_loop(self, filtered_image):  # probably needs to be video feed here
-        self.balls = detect_balls(filtered_image)
-
-    # get_image
-    # modify_image
-    # update_robot_position
-    # plan_path
-    # move_robot
-
-
-main = Main()
-main.main_loop()
-
-def get_image():
-    pass
-
-
-def modify_image(image):
-    pass
-
-
-def update_robot_position():
-    pass
-
-
-def plan_path():
-    pass
-
-
-def move_robot():
-    pass
+        if abs(deg) > TOLERANCE:
+            self.client.send_command(f"turn {deg}")
