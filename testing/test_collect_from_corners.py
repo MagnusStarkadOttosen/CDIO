f19f6d0018f5da95a7d8c8be586f41b52bcb52d5
NegarOstad <s224283@student.dtu.dk>
2024-06-11 09:01:20 +0200

    modify is_ball_in_corner and testing

--- testing/test_collect_from_corners.py
+++ testing/test_collect_from_corners.py
@@ -10,14 +10,21 @@ client_pc = ClientPC()
 CORNERS = [(0, 0), (0, 1200), (1800, 0), (1800, 1200)]
 PIVOT_POINTS = [(300, 600), (1500, 600)]
 IMAGE_SIZE = [1200, 1800]
-ball_coords = (1690, 574, 5)
+ball_coords_1 = (1690, 574, 5)
+ball_coords_2 = (1790, 10, 5)
 
 try:
     # corner_results = check_corners(ball_coords)
     # robot_movement_based_on_corners(corner_results)
-    if is_ball_in_corner(ball_coords):
-        corner_result = check_corners(ball_coords, threshold=50)
+    if is_ball_in_corner(ball_coords_1):
+        corner_result = check_corners(ball_coords_1, threshold=50)
         pivot_points, corner_points = robot_movement_based_on_corners(corner_result)
+
+    else:
+        print("Ball is not in any corners.")
+    if is_ball_in_corner(ball_coords_2):
+        corner_result_2 = check_corners(ball_coords_2, threshold=50)
+        pivot_points, corner_points = robot_movement_based_on_corners(corner_result_2)
 
     else:
         print("Ball is not in any corners.")
