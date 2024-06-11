f9f44e86bc1717b77679039e416408f2d8278c9c
Melissa Woo <s224311@dtu.dk>
2024-06-11 08:40:24 +0200

    removing unused test

--- testing/a_star_search_with_robot.py
+++
@@ -1,9 +0,0 @@
-from src.client.pc_client import ClientPC
-from src.client.search_targetpoint.a_star_search import find_path
-from src.mainloop import MainLoop
-
-main = MainLoop()
-client = ClientPC()
-# main._initialize_field()
-# path = [(3, 4), (5, 6), (6, 6), (8, 8), (8, 10)]
-# main._navigate_to_target(path)
