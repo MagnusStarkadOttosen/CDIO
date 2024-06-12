6644c8f83e458e4d6c616c367d3803d02499f8d6
Magnus Ottosen <magnus@ottosen.dk>
2024-06-12 13:01:42 +0200

    Test for navigating astar

--- testing/test_mainloop_functions.py
+++ testing/test_mainloop_functions.py
@@ -1,4 +1,6 @@
 from src.client.field.coordinate_system import warp_perspective
+from src.client.hsvLoad import read_hsv_values
+from src.client.pathfinding.GenerateNavMesh import GenerateNavMesh, cells_to_coordinates, coordinate_to_cell
 from src.client.search_targetpoint.a_star_search import find_path
 from src.mainloop import MainLoop
 from src.client.vision.shape_detection import detect_balls, detect_robot
@@ -31,9 +33,19 @@ def test_nav_to_target_detected_path(ml):
     print(f"robot_pos: {robot_pos}, robot_direction: {robot_direction}")
     ml.target_position = (ml.balls[0][0], ml.balls[0][1])
     print(f"target_position: {ml.target_position}")
-    path = find_path(ml.grid, robot_pos, ml.target_position)
+
+    red_hsv_values = read_hsv_values('hsv_presets_red.txt')
+    grid_navmesh = GenerateNavMesh(warped_img, red_hsv_values)
+
+    robotCell = coordinate_to_cell(robot_pos[0], robot_pos[1], 30)
+    targetCell = coordinate_to_cell(ml.target_position[0], ml.target_position[1], 30)
+
+    path = find_path(grid_navmesh, robotCell, targetCell)
     print(path)
-    ml._navigate_to_target(path)
+    newPath = cells_to_coordinates(path, 30)
+    print(newPath)
+
+    ml._navigate_to_target(newPath)
 
 
 def test_collect_nearest_ball(ml): # many white balls on field
@@ -43,7 +55,7 @@ def test_collect_nearest_ball(ml): # many white balls on field
 main_loop = MainLoop()
 main_loop.initialize_field()
 # main_loop._detect_initial_balls()
-test_nav_to_target_hardcoded_path(main_loop)
-# test_nav_to_target_detected_path(main_loop)
+# test_nav_to_target_hardcoded_path(main_loop)
+test_nav_to_target_detected_path(main_loop)
 # test_collect_nearest_ball(main_loop)
 
