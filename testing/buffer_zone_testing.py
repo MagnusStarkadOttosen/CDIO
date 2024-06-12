e9b78fcdacb8228ceeb152c13a152ab0d9d4d468
NegarOstad <s224283@student.dtu.dk>
2024-06-12 13:25:27 +0200

    test buffer zone

--- testing/buffer_zone_testing.py
+++ testing/buffer_zone_testing.py
@@ -9,15 +9,15 @@ main_loop.initialize_field()
 
 main_loop.client.send_command("start_drive")
 print("start_drive command sent")
-main_loop._navigate_to_target(robot_position_1)
-print("is within navigate to target zone")
-if is_within_buffer_zone(robot_position_1):
-        print("is within buffer zone")
-        main_loop.client.send_command("stop")
-        main_loop.client.send_command("move -20")
-        main_loop.client.send_command("stop")
-else:
-    main_loop.client.send_command("stop")
+# main_loop._navigate_to_target(robot_position_1)
+# print("is within navigate to target zone")
+# if is_within_buffer_zone(robot_position_1):
+#         print("is within buffer zone")
+#         main_loop.client.send_command("stop")
+#         main_loop.client.send_command("move -20")
+#         main_loop.client.send_command("stop")
+# else:
+#     main_loop.client.send_command("stop")
 
 main_loop._navigate_to_target(robot_position_2)
 if is_within_buffer_zone(robot_position_2):
