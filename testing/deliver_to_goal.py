5a54916e53973eb3e8c917d785fe7a62de336215
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-06-13 11:11:10 +0200

    DeliverBallsTest

--- testing/deliver_to_goal.py
+++ testing/deliver_to_goal.py
@@ -1,5 +1,8 @@
 from src.mainloop import MainLoop
+from testing.MoveAtoBVideo import client_pc
 
+turnSpeed = 3
 ml = MainLoop()
 ml.start_main_loop()
 ml._deliver_balls()
+client_pc.send_command(f"turn_left {-turnSpeed}")
