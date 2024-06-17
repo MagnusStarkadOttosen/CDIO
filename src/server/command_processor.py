e9af4c2747000d5108cf5fa7891ad908291d7719
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-06-17 10:15:10 +0200

    drive back with turn

--- src/server/command_processor.py
+++ src/server/command_processor.py
@@ -14,7 +14,7 @@ actions_functions = {
     "start_drive": commands.drive_inf,
     "turn_left": commands.turn_left,
     "drive_back": commands.drive_back,
-    "drive_back_save": commands.drive_back,
+    "drive_back_save": commands.drive_back_save,
     "drive_backward": commands.drive_backwards
 }
 
