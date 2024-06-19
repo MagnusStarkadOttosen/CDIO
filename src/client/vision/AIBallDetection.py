2b17acb1ac57185d8d7decda32c1d3bc620e74c2
Magnus Ottosen <magnus@ottosen.dk>
2024-06-19 15:11:03 +0200

    w

--- src/client/vision/AIBallDetection.py
+++ src/client/vision/AIBallDetection.py
@@ -3,6 +3,7 @@ import cv2
 import warnings
 from dotenv import load_dotenv
 import inference
+import numpy as np
 
 # Suppress specific warnings
 warnings.filterwarnings("ignore", message="Specified provider 'CUDAExecutionProvider'")
@@ -12,31 +13,24 @@ warnings.filterwarnings("ignore", message="Specified provider 'OpenVINOExecution
 load_dotenv()
 
 # Load the model once, so it doesn't reload every time the function is called
-model = inference.get_model("detect-balls/3")
+model = inference.get_model("detect-balls/5")
 
 def detect_balls_with_model(image, min_confidence=0.5):
-    # Convert the image to a format suitable for the model
-    temp_image_path = "temp_image.jpg"
-    cv2.imwrite(temp_image_path, image)
     
-    # Run inference on the image
-    results_list = model.infer(image=temp_image_path)
+    results_list = model.infer(image=image)
     
-    # List to store detected balls
-    detected_balls = []
+    # Lists to store detected balls
+    white_balls = []
+    orange_balls = []
 
     # Iterate over the list of results and collect detections
     for results in results_list:
         for prediction in results.predictions:
             if prediction.confidence >= min_confidence:
-                ball_data = {
-                    'x': prediction.x,
-                    'y': prediction.y,
-                    'width': prediction.width,
-                    'height': prediction.height,
-                    'confidence': prediction.confidence,
-                    'class_name': prediction.class_name,
-                }
-                detected_balls.append(ball_data)
+                ball_data = [int(prediction.x), int(prediction.y), 5]  # Set radius to 5
+                if prediction.class_name == 'white_ball':
+                    white_balls.append(ball_data)
+                elif prediction.class_name == 'orange_ball':
+                    orange_balls.append(ball_data)
     
-    return detected_balls
+    return np.array(white_balls, dtype=int), np.array(orange_balls, dtype=int)
