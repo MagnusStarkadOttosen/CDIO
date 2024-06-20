bb8c9297eba3946744308f71fa54b19bebb92fd6
Magnus Ottosen <magnus@ottosen.dk>
2024-06-20 15:04:20 +0200

    done hopefully

--- src/client/vision/shape_detection.py
+++ src/client/vision/shape_detection.py
@@ -10,7 +10,7 @@ from src.client.field.coordinate_system import find_intersection
 
 CAMERA_HEIGHT = 202
 ROBOT_HEIGHT = 23
-scale_factor = ROBOT_HEIGHT/CAMERA_HEIGHT
+scale_factor = (CAMERA_HEIGHT-ROBOT_HEIGHT)/CAMERA_HEIGHT
 def detect_robot(image, direction_color, pivot_color, center_point):
     direction_dot = detect_balls(filter_image(image, direction_color), min_radius=45, max_radius=50)
     if len(direction_dot) == 0:  # TODO Proper error handling for green_dot
@@ -25,14 +25,14 @@ def detect_robot(image, direction_color, pivot_color, center_point):
         return None, None
     print(f"what pivot detect finds {pivot_dot}")
     # print("yellow dot found ", len(pivot_dot))
-    cam_y, cam_x = (600,900)
+    cam_y, cam_x = center_point
 
-    real_pos_x = pivot_dot[0][0] - (cam_x + (pivot_dot[0][0]-cam_x) * scale_factor)
-    real_pos_y = pivot_dot[0][1] - (cam_y + (pivot_dot[0][1]-cam_y) * scale_factor)
+    real_pos_x = cam_x + (pivot_dot[0][0]-cam_x) * scale_factor
+    real_pos_y = cam_y + (pivot_dot[0][1]-cam_y) * scale_factor
     print(f"real_pos_x: {real_pos_x}, real_pos_y: {real_pos_y} , pivot:{pivot_dot}, {pivot_dot[0]}, pivot:{pivot_dot[0][0]}, {pivot_dot[0][1]}")
 
-    real_dir_x = direction_dot[0][0] - (cam_x + (direction_dot[0][0]-cam_x) * scale_factor)
-    real_dir_y = direction_dot[0][1] - (cam_y + (direction_dot[0][1]-cam_y) * scale_factor)
+    real_dir_x = cam_x + (direction_dot[0][0]-cam_x) * scale_factor
+    real_dir_y = cam_y + (direction_dot[0][1]-cam_y) * scale_factor
     print(f"real_dirx: {real_dir_x}, real_dir_y: {real_dir_y} , direction:{direction_dot}, {direction_dot[0]}, pivot:{direction_dot[0][0]}, {direction_dot[0][1]}")
     robot_pos = (real_pos_x, real_pos_y)
     robot_direction = calc_vector_direction((real_dir_x, real_dir_y), robot_pos)
