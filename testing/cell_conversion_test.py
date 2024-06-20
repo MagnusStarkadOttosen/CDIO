ba4a649e25ccdc813e422a24138d46e40b31c564
Melissa Woo <s224311@dtu.dk>
2024-06-20 14:33:48 +0200

    changing grid size to 25 and testing

--- testing/cell_conversion_test.py
+++ testing/cell_conversion_test.py
@@ -1,11 +1,20 @@
-from src.client.pathfinding.GenerateNavMesh import cells_to_coordinates
+from src.client.pathfinding.GenerateNavMesh import cells_to_coordinates, coordinate_to_cell
 
-converted_cell = cells_to_coordinates([(11, 20)], 30)
 
-print(converted_cell)
+grid_size = 25
+x0, y0 = 350, 600
+
+
+converted_coord = coordinate_to_cell(x0, y0, grid_size)
+print(f"converted coords: {converted_coord}")
+
+
+converted_cell = cells_to_coordinates([(converted_coord[0], converted_coord[1])], grid_size)
+print(f"converted cells: {converted_cell}")
 
 x, y = converted_cell[0][0], converted_cell[0][1]
 
 
 print(x)
 print(y)
+print(f"original_cells: {x0}, {y0}")
