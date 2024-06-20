f9c8b7ef0fb61d7e371ec5a49cb1923502d4af69
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-06-20 16:01:03 +0200

    logging cross buffer zone x and y

--- testing/collect_ball_in_obstacle_test.py
+++ testing/collect_ball_in_obstacle_test.py
@@ -2,7 +2,7 @@ import time
 import sys
 import os
 
-import cv2
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.client.hsvLoad import read_hsv_values
@@ -34,29 +34,32 @@ def test_collect_ball_in_obstacle(ml,camera, final_point, direction_color, pivot
     while robot_pos is None or robot_direction is None:
         robot_pos, robot_direction = detect_robot(warped_img, ml.direction_color, ml.pivot_color)
 
-    # white_hsv_values = read_hsv_values('hsv_presets_white.txt')
-    # red_hsv_values = read_hsv_values('hsv_presets_red.txt')
-    # navmesh = GenerateNavMesh(warped_img, red_hsv_values)
-    
-    # try:
-    #     filtered_img = filter_image(warped_img, hsv_values=white_hsv_values)
-    #     if filtered_img is None or filtered_img.size == 0:
-    #         print("Filtered image is empty.")
-    #         return
-    # except Exception as e:
-    #     print(f"Error in filtering image: {e}")
-    #     return
-    
-    # balls = detect_balls(filtered_img)
-    # if len(balls)<1:
-    #     print("No balls detected.")
-    #     return
-    # midpoint = detect_obstacles(filtered_img)
+    white_hsv_values = read_hsv_values('hsv_presets_white.txt')
+    red_hsv_values = read_hsv_values('hsv_presets_red.txt')
+    navmesh = GenerateNavMesh(warped_img, red_hsv_values)
+
+    try:
+        filtered_img = filter_image(warped_img, hsv_values=white_hsv_values)
+        if filtered_img is None or filtered_img.size == 0:
+             print("Filtered image is empty.")
+             return
+    except Exception as e:
+         print(f"Error in filtering image: {e}")
+         return
+
+    balls = detect_balls(filtered_img)
+    print(f"balls {balls}")
+    if len(balls)<1:
+        print("No balls detected.")
+        return
+
+    midpoint = detect_obstacles(filtered_img)
+    print(f"midpoint {midpoint}")
 # check if ball in balls is in obstacle
-    for ball in ml.balls:
-        if is_ball_in_obstacle(ball, ml.midpoint):
-            target_point, target = obstacle_Search(ball, ml.midpoint)
-            path = astar(ml.navmesh, robot_pos, target)
+    for ball in balls:
+        if is_ball_in_obstacle(ball, midpoint):
+            target_point, target = obstacle_Search(ball, midpoint)
+            path = astar(navmesh, robot_pos, target)
             # path = [target]
             print(f"path: {path} target point: {target_point} target: {target}")
             ml._navigate_to_target(path)
