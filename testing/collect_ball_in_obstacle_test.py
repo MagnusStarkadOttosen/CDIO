902316351d875a6246a4bcc8fe3140ad45382a74
Bayan <s224329@dtu.dk>
2024-06-21 11:50:46 +0200

    update

--- testing/collect_ball_in_obstacle_test.py
+++ testing/collect_ball_in_obstacle_test.py
@@ -58,7 +58,7 @@ def test_collect_ball_in_obstacle(ml):
 # check if ball in balls is in obstacle
     for ball in balls:
         in_obstacle, target_point, target = is_ball_in_obstacle(ball, midpoint)
-        in_buffer_zone,target_point = is_ball_in_buffer_zone(ball)
+
         if in_obstacle:
             path = [target_point]
             print(f"path: {path} target point: {target_point} target: {target}")
@@ -88,7 +88,8 @@ def test_collect_ball_in_obstacle(ml):
             ml.client.send_command("stop_collect")
             ml.client.send_command("stop")
             return
-        if in_buffer_zone:
+        if is_ball_in_buffer_zone(ball):
+            target_point= buffer_zone_search(ball)
             path = astar(ml.o4navmesh, robot_pos, target_point)
             ml._navigate_to_target(path)
             angle = rotate_vector_to_point(robot_pos, robot_direction, ball)
