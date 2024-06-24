d852f88b7dd41fb957fb208a82ceed0e4b9954da
Melissa Woo <s224311@dtu.dk>
2024-06-24 19:25:32 +0200

    checking out obstacle_search

--- src/client/search_targetpoint/obstacle_search.py
+++ src/client/search_targetpoint/obstacle_search.py
@@ -1,27 +1,30 @@
 # from client.vision.shape_detection import detect_obstacles
 
-def obstacle_Search(ball_dot, i, j, midpoint):
-    target_point = [0, 0]
-    if ball_dot[i] < midpoint[i] and ball_dot[j] < midpoint[j]:
-        target_point = [midpoint[0] - 95, midpoint[1] - 320]  
-        target=   [midpoint[0] - 95, midpoint[1] + 320]  
-    elif ball_dot[i] < midpoint[i] and ball_dot[j] > midpoint[j]:
-        target_point = [midpoint[0] - 95, midpoint[1] + 320]  
-        target=   [midpoint[0] - 95, midpoint[1] - 320]     
-    elif ball_dot[i] > midpoint[i] and ball_dot[j] > midpoint[j]:
-        target_point = [midpoint[0] + 95, midpoint[1] - 320] 
-        target=   [midpoint[0] + 95, midpoint[1] + 320]       
-    elif ball_dot[i] > midpoint[i] and ball_dot[j] < midpoint[j]:
-        target_point = [midpoint[0] + 95, midpoint[1] + 320]   
-        target=   [midpoint[0] + 95, midpoint[1] - 320]   
+def obstacle_Search(ball_dot, midpoint):
+    target_point = (0,0)
+    target = (0,0)
+    if ball_dot[0] < midpoint[0] and ball_dot[1] < midpoint[1]:
+        target_point = (midpoint[0] - 95, midpoint[1] - 320)
+        target= (midpoint[0] - 95, midpoint[1] + 200)
+    elif ball_dot[0] < midpoint[0] and ball_dot[1] > midpoint[1]:
+        target_point = (midpoint[0] - 85, midpoint[1] + 320)
+        target=(midpoint[0] - 95, midpoint[1] - 200)
+    elif ball_dot[0] > midpoint[0] and ball_dot[1] > midpoint[1]:
+        target_point = (midpoint[0] + 95, midpoint[1] + 320)
+        target= (midpoint[0] + 95, midpoint[1] - 200)
+    elif ball_dot[0] > midpoint[0] and ball_dot[1] < midpoint[1]:
+        target_point = (midpoint[0] + 95, midpoint[1] - 320)
+        target=(midpoint[0] + 95, midpoint[1] + 200)
+
     return target_point, target
 
-def is_ball_in_obstacle(ball_coords, midpoint):
-    if(abs(ball_coords[0]-midpoint[0])<100 and abs(ball_coords[1]-midpoint[1])<100):
-        target_point = obstacle_Search(ball_coords, 0, 1, midpoint)
-        target = obstacle_Search(ball_coords, 1, 0, midpoint)
+def is_ball_in_obstacle(ball_dot, midpoint):
+    print(f"ball_coords: {ball_dot} midpoint: {midpoint}")
+    if(abs(ball_dot[0]-midpoint[0])<100 and abs(ball_dot[1]-midpoint[1])<100):
+        target_point,target = obstacle_Search(ball_dot, midpoint)
+        print(f"target_point: {target_point} target:{target}bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
         return True, target_point, target
     return False, None, None
-
+    
 
     
